17cb7e0c7c832c93ab9cce5a907f652f6b0c3330
Leandro Ortiz <lortiz@conect-it.com.ar>
2021-11-10 20:30:06 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express =require ("express");
+const path= require("path")
+const app =express();
+
+
+
+const publicPath= path.resolve(__dirname, "./public")
+app.use(express.static(publicPath));
+
+app.listen(process.env.PORT || 3000,()=>{
+    console.log("Servidor corriendo")
+});
+
+app.get ("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/home.html"))
+})
+
+app.get ("/formulario",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/formulario.html"))
+})
+
+// app.post("/formulario",(req,res))=>{
+//     res.redirect("/");
+// })
